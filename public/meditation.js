f85e5d30341608e2df023975a0eebe0584d816bc
Susan Buggell <susanbuggell@Susans-MBP.lan>
2022-01-19 12:37:09 -0500

    update added *Products.JSON to public

--- public/meditation.js
+++ public/meditation.js
@@ -1,11 +1,14 @@
 // import axios from "axios"
 
-// const btnAddMedEvent1 = document.getElementById(add-cart-med-event1)
-// const btnAddMedEvent2 = document.getElementById(add-cart-med-event2)
+// const btnAddMedEvent1 = document.getElementById('add-cart-med-event1')
+// const btnAddMedEvent2 = document.getElementById('add-cart-med-event2')
 // const meditationProducts = axios.get('meditationProducts.value')
 
 console.log('meditationProducts.data')
 
+axios.get('./public/products/meditateProducts.JSON')
+.then(res => console.log(res.data))
+.catch(err => console.log(err))
 
 // handlerAddEventToCard()
 // btnAddMedEvent1.addEventListener('click',<insert function></insert>)
--- public/yoga.js
+++ public/yoga.js
@@ -1,1 +1,5 @@
 console.log("yoga.js")
+
+axios.get('./public/yogaProducts.JSON')
+.then(res => console.log(res.data))
+.catch(err => console.log(err))
