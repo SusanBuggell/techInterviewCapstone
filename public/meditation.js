3a54740acb333bc4ecad29da92fc8e60f3896049
Susan Buggell <susanbuggell@Susans-MBP.lan>
2022-01-19 12:25:24 -0500

    update

--- public/meditation.js
+++ public/meditation.js
@@ -1,12 +1,12 @@
-import axios from "axios"
+// import axios from "axios"
 
-const btnAddMedEvent1 = document.getElementById(add-cart-med-event1)
-const btnAddMedEvent2 = document.getElementById(add-cart-med-event2)
-const meditationProducts = axios.get('meditationProducts.value')
+// const btnAddMedEvent1 = document.getElementById(add-cart-med-event1)
+// const btnAddMedEvent2 = document.getElementById(add-cart-med-event2)
+// const meditationProducts = axios.get('meditationProducts.value')
 
 console.log('meditationProducts.data')
 
 
-handlerAddEventToCard()
-btnAddMedEvent1.addEventListener('click',<insert function></insert>)
-btnAddMedEvent2.addEventListener
+// handlerAddEventToCard()
+// btnAddMedEvent1.addEventListener('click',<insert function></insert>)
+// btnAddMedEvent2.addEventListener
